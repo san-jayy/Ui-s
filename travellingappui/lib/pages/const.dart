49966fcc5b1957ac64b03149474eae5a924d89d1
san-jayy <sanjay.space.66@gmail.com>
2022-08-23 19:16:05 +0530

    added constants

---
+++ travellingappui/lib/pages/const.dart
@@ -0,0 +1,19 @@
+import 'package:flutter/material.dart';
+import 'package:google_fonts/google_fonts.dart';
+
+final kprimaryTextStyle = GoogleFonts.montserrat(
+  fontSize: 20,
+  color: Colors.black,
+  fontWeight: FontWeight.bold,
+);
+
+final ksecondaryTextStyle = GoogleFonts.montserrat(
+  fontSize: 14,
+  color: Colors.black,
+  fontWeight: FontWeight.bold,
+);
+
+final ktertiaryTextStyle = GoogleFonts.montserrat(
+  fontSize: 14,
+  color: Colors.grey[800],
+);
